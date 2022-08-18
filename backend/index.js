7e00f176fa5b1610c4120046b0b79a50b271f3ed
HolyOly <olvstep@mail.ru>
2022-08-18 14:50:57 +0300

    feat: add cors

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,23 @@
 import exppress from 'express';
 import mongoose from 'mongoose';
+// import cors from 'cors';
+import cors from 'cors';
 import router from './registration/router.js';
 
 const port = 5000;
 const LOGIN_PASSWORD = 'admin:admin';
 const URL_DB = `mongodb+srv://${LOGIN_PASSWORD}@atlascluster.hi1wyqs.mongodb.net/?retryWrites=true&w=majority`;
 
+// const cors = require('cors');
+
 const app = exppress();
 app.use(exppress.json());
 app.use('/api', router);
+app.use(cors({
+  methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
+  origin: '*',
+}));
+// app.options('*', cors());
 // app.use('/users', router); // create userrouter
 
 async function startClone() {
