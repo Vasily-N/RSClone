0ae3ee7d0ba8be5f7c9d1cd4834b9733ddd37d1b
HolyOly <olvstep@mail.ru>
2022-08-24 14:45:11 +0300

    feat: rename files and change RegWindow class

--- frontend/src/views/auth/index.ts
+++ frontend/src/views/auth/index.ts
@@ -1,21 +1,38 @@
-import template from './index.html';
+import template from './registerWindow.html';
 import style from './auth.scss';
 import { View } from '..';
 import { ApiServices } from '../../services';
 
+export interface UserData {
+  name: string,
+  password: string,
+}
+
 class RegWindow extends View {
   private readonly regWin: HTMLDivElement;
+  private readonly regBtn: HTMLButtonElement;
   services: ApiServices;
 
   private initRegWindow():HTMLDivElement {
     const reg = this.getElementById('reg') as HTMLDivElement;
+    // this.regBtn.addEventListener('click', apiServices??);
     return reg;
+  }
+
+  private getUserData():UserData {
+    const nameEl = this.getElementById('name-reg') as HTMLInputElement;
+    const passEl = this.getElementById('pass-reg') as HTMLInputElement;
+    return {
+      name: nameEl.value,
+      password: passEl.value,
+    };
   }
 
   constructor(parentId:string, services: ApiServices) {
     super(parentId, template, style);
     this.services = services;
     this.regWin = this.initRegWindow();
+    this.regBtn = this.getElementById('reg-user') as HTMLButtonElement;
   }
 }
 
